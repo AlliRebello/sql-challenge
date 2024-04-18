653cb906fde799bb94d12fccbfefbaf43d8b0de1
AlliRebello <148725595+AlliRebello@users.noreply.github.com>
2024-04-18 19:08:39 -0400

    Add files via upload

--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -1,62 +1,51 @@
---List the employee number, last name, first name, sex, and salary of each employee
-SELECT e.emp_no, e.first_name, e.last_name, e.sex, s.salary
-FROM employees AS e
-LEFT JOIN salaries AS s
-ON e.emp_no = s.emp_no;
+-- this make sure that all csv file that is imported is in the correct format.
+ALTER DATABASE table_schemata SET datestyle TO "ISO, MDY";
 
---List the first name, last name, and hire date for the employees who were hired in 1986
-SELECT last_name, first_name, hire_date
-FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+CREATE TABLE titles (
+    title_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    PRIMARY KEY (title_id)
+);
 
---List the manager of each department along with their department number, department name, 
---employee number, last name, and first name
-SELECT d_m.dept_no, d.dept_name, e.first_name, e.last_name
-FROM dept_manager AS d_m
-JOIN  departments AS d
-ON d_m.dept_no = d.dept_no
-JOIN  employees AS e
-ON d_m.emp_no = e.emp_no;
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sex VARCHAR(50) NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
 
---List the department number for each employee along with that employee’s employee number, 
---last name, first name, and department name
-SELECT d_e.dept_no, d.dept_name, e.first_name, e.last_name
-FROM employees AS e
-JOIN dept_emp AS d_e
-ON e.emp_no = d_e.emp_no
-JOIN departments AS d
-ON d_e.dept_no = d.dept_no
-ORDER BY d.dept_no;
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
---List first name, last name, and sex of each employee whose first name is Hercules 
---and whose last name begins with the letter B
-SELECT first_name, last_name, sex
-FROM employees
-WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+-- dept_no column is already defined as the primary key, it cannot be used again as a unique constraint. Therefore, we use the UNIQUE constraint on the dept_name column to ensure data integrity 
+CREATE TABLE departments (
+    dept_no VARCHAR(255) NOT NULL,
+    dept_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (dept_no),
+    UNIQUE (dept_name) 
+);
 
---List each employee in the Sales department, including their employee number, last name, and first name
-SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
-FROM employees AS e
-JOIN dept_emp AS d_e
-ON e.emp_no = d_e.emp_no
-JOIN departments AS d
-ON d_e.dept_no = d.dept_no
-WHERE dept_name = 'Sales'
+-- dept_no and emp_no are not foreign keys but rather they are used to define a composite primary key. This means that both columns together uniquely identify each row in the table.
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(255) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
 
---List each employee in the Sales or Development departments, including their employee number, 
---last name, first name, and department name 
-SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
-FROM employees AS e
-JOIN dept_emp AS d_e
-ON e.emp_no = d_e.emp_no
-JOIN departments AS d
-ON d_e.dept_no = d.dept_no
-WHERE dept_name = 'Sales' OR dept_name = 'Development'
-
---List the frequency counts, in descending order, of all the employee last names 
---(that is, how many employees share each last name)
-SELECT last_name, COUNT(*)
-FROM employees
-GROUP BY  last_name 
-ORDER BY COUNT(*) DESC
-
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(255)   NOT NULL,
+    emp_no INT NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
